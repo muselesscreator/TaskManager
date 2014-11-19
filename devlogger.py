f8f0fb6ca8e48146ff6e39aca5e5a710f0b87b97
Ben Warzeski <bwarzesk@gmail.com>
2014-11-19 07:40:38 -0500

    added devlog and tasks for 11/19/2014

---
+++ devlogger.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+
+import os
+import sys
+import time
+import argparse
+
+def devlog(text):
+    folder_path = os.path.join(os.path.expanduser('~'), 'devlogs')
+    day_str = time.strftime("%A_%b%w_%Y")
+    time_str = time.strftime("%I:%M %p")
+    fn = '%s/%s.txt' % (folder_path, day_str)
+    max_len = 69
+    text_sections = []
+    if len(text) <= max_len:
+        text_sections = [text]
+    else:
+        words = text.split()
+        while len(words):
+            for i in range(len(words)):
+                if len(' '.join(words[:i+1])) > max_len:
+                    ind = i
+                    break
+            text_sections.append(' '.join(words[:ind]))
+            words = words[ind:]
+
+    final_text = '%s - %s\n' % (time_str, text_sections[0])
+
+    if len(text_sections) > 1:
+        final_text = '%s%s\n' % (final_text, '\n'.join(["%s%s" % ('           ', section) for section in text_sections[1:]]))
+
+    with open(fn, 'a') as f:
+       f.write(final_text)
